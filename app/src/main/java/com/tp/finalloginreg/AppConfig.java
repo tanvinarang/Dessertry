65f398d5ccc519338569be8a9a83dd1368f2d818
tanvinarang <narangtanvi19@gmail.com>
2016-04-17 04:48:20 +0530

    Initial commit

---
+++ app/src/main/java/com/tp/finalloginreg/AppConfig.java
@@ -0,0 +1,11 @@
+package com.tp.finalloginreg;
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://dessertry.comlu.com/login.php" ;
+
+    // Server user register url
+    public static String URL_REGISTER = "http://dessertry.comlu.com/register.php";
+}
+
+
+
